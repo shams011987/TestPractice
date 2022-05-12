0242cb9d60b655344197d32fccf69b6f9c338ff0
[shams011987] <[shamsuddin.malikzada@gmail.com]>
2022-05-12 16:19:34 -0700

    lab session first commit

--- src/GitHubShark.java
+++ src/GitHubShark.java
@@ -4,6 +4,10 @@ public class GitHubShark {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int a = 20;
+		int b = 10;
+		int c = a-b;
+		System.out.println(c);
 	}
 
 }
